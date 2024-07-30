1a581cd61c1091086b3b152e7238878c633189bf
vishnunair279 <vishnunair279@gmail.com>
2024-07-30 14:02:45 +0530

    Added routing logic in GatewayserverApplication.java

--- gatewayserver/src/main/java/com/vishnu/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/vishnu/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,33 @@ package com.vishnu.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+
+	@Bean
+	public RouteLocator bankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(r -> r
+						.path("/bank/accounts/**")
+						.filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(r -> r
+						.path("/bank/loans/**")
+						.filters(f -> f.rewritePath("/bank/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.route(r -> r
+						.path("/bank/cards/**")
+						.filters(f->f.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS")).build();
 	}
 
 }
