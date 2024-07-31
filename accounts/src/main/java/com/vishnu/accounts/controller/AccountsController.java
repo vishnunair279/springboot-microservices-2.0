eb38eeca1bdd234b6d749802164932c6c9a291f8
vishnunair279 <vishnunair279@gmail.com>
2024-08-01 02:02:18 +0530

    Implemented rate limiter in accounts service for the api "/java-version".

--- accounts/src/main/java/com/vishnu/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/vishnu/accounts/controller/AccountsController.java
@@ -7,6 +7,7 @@ import com.vishnu.accounts.dto.ErrorResponseDto;
 import com.vishnu.accounts.dto.ResponseDto;
 import com.vishnu.accounts.service.IAccountsService;
 import com.vishnu.accounts.constants.AccountsConstants;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -233,11 +234,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
