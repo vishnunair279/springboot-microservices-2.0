aedd153b8a90649f8e83da7d273119d2e2b9ffd6
vishnunair279 <vishnunair279@gmail.com>
2024-07-31 20:15:31 +0530

    Configured default timeout behaviour of CircuitBreaker.

--- gatewayserver/src/main/java/com/vishnu/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/vishnu/gatewayserver/GatewayserverApplication.java
@@ -1,7 +1,12 @@
 package com.vishnu.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -41,6 +46,15 @@ public class GatewayserverApplication {
 						.filters(f->f.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}")
 									 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://CARDS")).build();
+	}
+
+
+//	 To override the default timeout behaviour of CircuitBreaker. eg: wait 4 secs before throwing a timeout.
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
 	}
 
 }
